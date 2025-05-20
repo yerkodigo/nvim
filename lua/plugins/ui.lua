8ec30a38b2a7be521bcc934d844727e566965571
Yerko Andres Cortes Baeza <yerko.cortes@evertecinc.com>
2025-05-20 09:23:21 -0400

    Agrega lualine para información barra inferior

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -32,7 +32,37 @@ return {
       highlight LineNr guibg=none
       ]])
     end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- para iconos opcionales
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "onelight", -- respeta los colores del colorscheme actual
+          section_separators = "",
+          component_separators = "",
+          globalstatus = true, -- barra única en lugar de una por ventana
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+          lualine_c = { { "filename", path = 1 } },
+          lualine_x = { "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        extensions = {},
+      })
+    end,
   }
-
 }
-
