d4e80f190432df9d3735eaa6d2e3a5fe49704943
Yerko Cortés Baeza <ykacorb@gmail.com>
2025-05-19 22:20:58 -0400

    Agrega movimiento entre tabs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>nt", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+
+-- Moverse entre ventanas con Ctrl + h/j/k/l
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Mover foco a la ventana izquierda" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Mover foco a la ventana derecha" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Mover foco a la ventana abajo" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Mover foco a la ventana arriba" })
+
