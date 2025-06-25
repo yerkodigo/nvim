4a35110039e2a43362d0346dbe78b5346b59e18f
Yerko Cortés Baeza <ykacorb@gmail.com>
2025-06-25 15:11:57 -0400

    Configura git

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,7 +8,13 @@ nvim_lsp.angularls.setup{} -- Angular (requiere @angular/language-server)
 
 -- Vue (Volar)
 nvim_lsp.volar.setup({
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue'},
+  filetypes = {
+    'typescript',
+    'javascript',
+    'javascriptreact',
+    'typescriptreact',
+    'vue'
+  },
   init_options = {
     vue = {
       hybridMode = false,
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,22 +2,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup({
-        signs = {
-          add          = { text = "+" },
-          change       = { text = "~" },
-          delete       = { text = "-" },
-          topdelete    = { text = "‾" },
-          changedelete = { text = "~-" },
-        },
-        -- Opcional: actualiza más rápido al guardar archivos
-        update_debounce = 100,
-        watch_gitdir = {
-          interval = 1000,
-          follow_files = true,
-        }
-      })
+      require("config.git").setup()
     end,
   },
 }
-
