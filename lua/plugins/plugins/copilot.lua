1b64bfb1332d0b4d82db5bbc56837d20e44e885e
Yerko Cortés Baeza <ykacorb@gmail.com>
2025-08-04 11:45:55 -0400

    Agrega Copilot

---
+++ lua/plugins/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+
+      vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { expr = true, silent = true, desc = "Aceptar sugerencia Copilot con Ctrl+j" })
+      -- vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")', { expr = true, silent = true, desc = "Aceptar sugerencia Copilot con Tab" })
+
+      vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Next()', { expr = true, silent = true, desc = "Siguiente sugerencia Copilot con Ctrl+n" })
+      vim.api.nvim_set_keymap("i", "<S-Tab>", 'copilot#Previous()', { expr = true, silent = true, desc = "Sugerencia previa Copilot con Ctrl+p" })
+
+    end,
+  },
+}
