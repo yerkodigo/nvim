f80656ccafa76ae1561f2bbd0a110bf1c736b3d1
Yerko Cortés Baeza <ykacorb@gmail.com>
2025-08-05 10:26:30 -0400

    Ajusta ruta que se muestra en búsqueda de archivos

--- lua/plugins/plugins/telescope.lua
+++ lua/plugins/plugins/telescope.lua
@@ -9,17 +9,16 @@ return {
       "folke/todo-comments.nvim"
     },
     config = function()
-      -- require("telescope").setup {}
       local telescope = require("telescope")
       local actions = require("telescope.actions")
 
       vim.keymap.set('n', '<leader>gs', require("telescope.builtin").git_status)
       vim.keymap.set('n', '<leader>gd', ":Gitsigns diffthis<CR>")
-      
+
       telescope.setup({
 
-        defaults= {
-          path_display = { "smart" },
+        defaults = {
+          path_display = { "relative" },
           mappings = {
             i = {
               ["<C-k>"] = actions.move_selection_previous,
@@ -36,15 +35,11 @@ return {
       -- keymaps
       local keymap = vim.keymap
 
-      keymap.set("n", "<leader>p", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd"})
-      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files"})
-      keymap.set("n", "<leader>ag", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd"})
-      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd"})
-      keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find TODOS"})
-
+      keymap.set("n", "<leader>p", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      keymap.set("n", "<leader>ag", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+      keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find TODOS" })
     end,
   },
 }
-
-
-
