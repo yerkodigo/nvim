eedf0c85b616a138e717fae9f75c7b8018611246
Yerko Cortés baeza <ykacorb@gmail.com>
2025-10-26 20:52:13 -0300

    Actualiza cambio de colorscheme con hora del sistema

--- lua/plugins/plugins/colorscheme.lua
+++ lua/plugins/plugins/colorscheme.lua
@@ -18,21 +18,35 @@ return {
     },
     config = function(_, opts)
       local vi = vim
-      local colorscheme_name = "tron"
+      local HORA_FRONTERA = 19
+
+      local function get_colorscheme_by_time()
+        local hora_actual = tonumber(os.date("%H"))
+
+        if hora_actual >= HORA_FRONTERA then
+          return "tron", "dark"
+        else
+          -- Modo diurno (< 19:00)
+          return "flynn", "light"
+        end
+      end
+
+      local colorscheme_name, background = get_colorscheme_by_time()
 
       vi.opt.termguicolors = true
-      vi.opt.background = opts.background or "light"
+      vi.opt.background = background
 
       vi.cmd('colorscheme ' .. colorscheme_name)
 
-      local function set_colorscheme(style)
+      local function set_colorscheme(scheme, style)
         vi.opt.background = style
-        vi.cmd('colorscheme ' .. colorscheme_name)
+        vi.cmd('colorscheme ' .. scheme)
       end
 
-      vi.keymap.set('n', '<leader>dt', function() set_colorscheme('dark') end,
-        { desc = colorscheme_name:gsub("^%l", string.upper) .. ' Dark' })
-      vi.keymap.set('n', '<leader>lt', function() set_colorscheme('light') end,
-        { desc = colorscheme_name:gsub("^%l", string.upper) .. ' Light' })
+      vi.keymap.set('n', '<leader>dt', function() set_colorscheme('tron', 'dark') end,
+        { desc = 'Tron Dark' })
+      vi.keymap.set('n', '<leader>lt', function() set_colorscheme('flynn', 'light') end,
+        { desc = 'Flynn Light' })
     end,
+  }
 }
