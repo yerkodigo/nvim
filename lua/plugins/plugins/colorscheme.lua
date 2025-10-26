7cd9a94e0b92fc78b1d1c0196d51ecc23069e6d6
Yerko Cortés baeza <ykacorb@gmail.com>
2025-10-26 18:45:45 -0300

    Agrega nuevo colorscheme

--- lua/plugins/plugins/colorscheme.lua
+++ lua/plugins/plugins/colorscheme.lua
@@ -1,36 +1,38 @@
 return {
   {
     -- DEV
-    -- "ykacolors-local",
-    -- dir = vim.fn.expand("~/.config/ykacolors.vim"), -- Use vim.fn.expand
+    -- "yka-tron-local",
+    -- dir = vim.fn.expand("~/.config/yka.tron"),
     -- dev = true,
 
     -- REPO
-    "yerkodigo/yka.mono",
-    name = "yka-mono",
-    branch = 'dev',
+    "yerkodigo/yka.tron",
+    name = "yka-tron",
+    branch = 'main',
 
     lazy = false,
     priority = 1000,
     opts = {
-      background = "light",
+      background = "dark",
       transparent = false,
     },
     config = function(_, opts)
       local vi = vim
+      local colorscheme_name = "tron"
 
       vi.opt.termguicolors = true
       vi.opt.background = opts.background or "light"
 
-      vi.cmd('colorscheme ykamono')
+      vi.cmd('colorscheme ' .. colorscheme_name)
 
-      local function set_ykamono(style)
+      local function set_colorscheme(style)
         vi.opt.background = style
-        vi.cmd('colorscheme ykamono')
+        vi.cmd('colorscheme ' .. colorscheme_name)
       end
 
-      vi.keymap.set('n', '<leader>dt', function() set_ykamono('dark') end, { desc = 'YkaMono Dark' })
-      vi.keymap.set('n', '<leader>lt', function() set_ykamono('light') end, { desc = 'YkaMono Light' })
+      vi.keymap.set('n', '<leader>dt', function() set_colorscheme('dark') end,
+        { desc = colorscheme_name:gsub("^%l", string.upper) .. ' Dark' })
+      vi.keymap.set('n', '<leader>lt', function() set_colorscheme('light') end,
+        { desc = colorscheme_name:gsub("^%l", string.upper) .. ' Light' })
     end,
-  },
 }
