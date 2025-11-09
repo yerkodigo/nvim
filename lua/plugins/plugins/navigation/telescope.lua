8dfda15ee871e9f92d570b62659152e510bcdf2f
Yerko Cortés baeza <ykacorb@gmail.com>
2025-11-09 17:12:14 -0300

    Agrega visualización de commits en archivo

--- lua/plugins/plugins/navigation/telescope.lua
+++ lua/plugins/plugins/navigation/telescope.lua
@@ -16,6 +16,7 @@ return {
       { "<leader>fc", "<cmd>Telescope grep_string<cr>", desc = "Find string under cursor in cwd" },
       { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find TODOS" },
       { "<leader>gs", function() require("telescope.builtin").git_status() end, desc = "Git status" },
+      { "<leader>gc", function() require("telescope.builtin").git_bcommits() end, desc = "Git commits (buffer)" },
     },
     config = function()
       local telescope = require("telescope")
@@ -29,6 +30,50 @@ return {
               ["<C-k>"] = actions.move_selection_previous,
               ["<C-j>"] = actions.move_selection_next,
               ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            }
+          }
+        },
+        pickers = {
+          git_bcommits = {
+            mappings = {
+              i = {
+                ["<CR>"] = function(prompt_bufnr)
+                  local selection = require("telescope.actions.state").get_selected_entry()
+                  local current_file = vim.fn.expand("%:p")
+                  actions.close(prompt_bufnr)
+
+                  -- Get the diff content
+                  local diff = vim.fn.system("git show " .. selection.value .. " -- " .. current_file)
+
+                  -- Open in new split with diff content (read-only)
+                  vim.cmd("vsplit")
+                  local buf = vim.api.nvim_create_buf(false, true)
+                  vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(diff, "\n"))
+                  vim.api.nvim_win_set_buf(0, buf)
+                  vim.bo[buf].filetype = "diff"
+                  vim.bo[buf].modifiable = false
+                  vim.bo[buf].buftype = "nofile"
+                end,
+              },
+              n = {
+                ["<CR>"] = function(prompt_bufnr)
+                  local selection = require("telescope.actions.state").get_selected_entry()
+                  local current_file = vim.fn.expand("%:p")
+                  actions.close(prompt_bufnr)
+
+                  -- Get the diff content
+                  local diff = vim.fn.system("git show " .. selection.value .. " -- " .. current_file)
+
+                  -- Open in new split with diff content (read-only)
+                  vim.cmd("vsplit")
+                  local buf = vim.api.nvim_create_buf(false, true)
+                  vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(diff, "\n"))
+                  vim.api.nvim_win_set_buf(0, buf)
+                  vim.bo[buf].filetype = "diff"
+                  vim.bo[buf].modifiable = false
+                  vim.bo[buf].buftype = "nofile"
+                end,
+              }
             }
           }
         }
